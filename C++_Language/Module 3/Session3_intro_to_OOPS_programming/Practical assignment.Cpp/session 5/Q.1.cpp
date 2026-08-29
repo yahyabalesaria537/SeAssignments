92b5dd509d4d3a661f0b58f7b70b11b10f42ae6f
unknown <yahyabalesaria537@gmail.com>
2026-08-29 16:36:45 +0530

    Add session 7 practical assignment

--- C++_Language/Module 3/Session3_intro_to_OOPS_programming/Practical assignment.Cpp/session 5/Q.1.cpp
+++ C++_Language/Module 3/Session3_intro_to_OOPS_programming/Practical assignment.Cpp/session 5/Q.1.cpp
@@ -1,17 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class paymentprocessor{
-	public:
-	void processpayment(double amount){
-		cout<<"\n version: without coupoun | final amount: "<<amount;
-	}
-	void processpayment(double amount,const char* coupouncode){
-		double finalamount =amount-50.0;
-		cout<<"\n version: with coupoun("<<coupouncode<<")|final amount:"<<finalamount;
-	}
+class Song{
+private:
+    char title[50];
+    char artist[50];
+public:
+    void setTitle(const char t[]){
+        int i = 0;
+        while (t[i]!='\0'){
+            title[i]=t[i];
+            i++;
+        }
+        title[i]='\0';
+    }
+    const char*getTitle(){
+        return title;
+    }
+    void setArtist(const char a[]){
+        int i=0;
+        while(a[i]!='\0'){
+            artist[i]=a[i];
+            i++;
+        }
+        artist[i]='\0';
+    }
+    const char*getArtist(){
+        return artist;
+    }
 };
 main(){
-	paymentprocessor p1;
-	p1.processpayment(600.0);
-	p1.processpayment(600.0,"SAVE50");
+    Song s1;
+    s1.setTitle("Tu hai ke nahi");
+    s1.setArtist("Arjit singh");
+    cout<<"Original Title: "<<s1.getTitle()<<endl;
+    s1.setTitle("Perfect");
+    cout<<"Updated Title: "<<s1.getTitle()<<endl;
 }
